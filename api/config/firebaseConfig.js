7c0c9e64f3d82564bfb7f7fd8376c88c7cfd1f52
rasyidrizky <rasyid.rsn@gmail.com>
2025-10-09 19:05:15 +0700

    fix: firebase

--- api/config/firebaseConfig.js
+++ api/config/firebaseConfig.js
@@ -1,14 +1,18 @@
-import admin from 'firebase-admin';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const firebaseConfig = {
+    apiKey: "AIzaSyCQaTgDfU8aOklhLNZxu0jfAGQYkpjbz3Q",
+    authDomain: "pawlearn.firebaseapp.com",
+    projectId: "pawlearn",
+    storageBucket: "pawlearn.firebasestorage.app",
+    messagingSenderId: "775306104507",
+    appId: "1:775306104507:web:dc23fe6e1c6e1cc683a39e",
+    measurementId: "G-279Y771B0X"
+};
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
-
-const auth = admin.auth();
-const db = admin.firestore();
-
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 export { auth, db };
